7c0e5fdbfb19f39a938538e270381314e8a5f5b8
Becky Nisttahuz <benistta014@gmail.com>
2024-04-26 12:26:25 -0600

    Completed glab 305.5.1B ManyToOne and OneToMany

---
+++ src/main/java/model/Teacher.java
@@ -0,0 +1,62 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Teacher implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+//    do we need this?
+    @GeneratedValue( strategy=GenerationType.IDENTITY )
+    private int teacherId;
+    private String salary;
+    private String TeacherName;
+
+    @ManyToOne
+    private Department department;
+//    @JoinColumn(name = "fk_dep")
+//    private Department department;
+    public Teacher(String salary, String teacherName, Department Dept){
+        this.salary = salary;
+        this.TeacherName = teacherName;
+        this.department = Dept;
+    }
+//    public Teacher(int teacherId, String salary, String teacherName) {
+//        super();
+//        this.teacherId = teacherId;
+//        this.salary = salary;
+//        TeacherName = teacherName; }
+//    public Teacher() {}
+
+//    public Teacher(String s, String mHaseeb, Department dept1) {
+//    }
+
+    public Department getDep() {
+        return department; }
+    public void setDep(Department department) {
+        this.department = department;
+    }
+    public int getTeacherId() {
+        return teacherId;
+    }
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+    public String getSalary() {
+        return salary;
+    }
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+    public String getTeacherName() {
+        return TeacherName;
+    }
+    public void setTeacherName(String teacherName) {
+        TeacherName = teacherName; }
+}
+
